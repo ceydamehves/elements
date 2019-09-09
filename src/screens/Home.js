aad6a860bfa83edf9e37561342726efe8cc32f26
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-09-10 00:17:12 +0300

    gradient theme and new page added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,45 +3,44 @@ import {ThemeProvider , Button , Header} from 'react-native-elements';
 import {headerStyles as styles} from 'C:/elements/src/styles.js';
 import {TouchableOpacity , Text , Dimensions } from 'react-native';
 import { Icon } from 'react-native-elements';
-
+import LinearGradient from 'react-native-linear-gradient';
 
 const theme ={
     Button: {
         type : "outline",
         
         titleStyle: {
-            color: 'magenta',
+            color: 'black',
+            size: 48
         },
     },
 };
 
-class HomeScreen extends React.Component{
+class Home extends React.Component{
     render (){
        
         return(
             <ThemeProvider theme = {theme}>
                   
-            <Header> 
-                <Icon 
-                    name='arrow-back'
-                    iconStyle={{ icon: 'arrow-back', color: '#fff', marginBottom: 20 }} 
-                    onPress={() => {
-                        this.props.navigation.navigate ('Home')           
-                    }}></Icon> 
-                <Icon 
-                    name='home'
-                    iconStyle={{ icon: 'home', color: '#fff', marginBottom: 20 }} 
-                    onPress={() => {
-                        this.props.navigation.navigate ('Home')           
-                    }}></Icon>          
+            <Header
+                ViewComponent={LinearGradient} // Don't forget this!
+                linearGradientProps={{
+                colors: ['magenta', 'pink'],
+                start: { x: 0, y: 0.5 },
+                end: { x: 1, y: 0.5 },
+            }}>        
                 <Icon 
                     name='menu'
                     iconStyle={{ icon: 'menu', color: '#fff', marginBottom: 20}} 
                     onPress={() => {
                         this.props.navigation.navigate ('firstpage')           
                     }}></Icon>
-                
-                    
+                <Button
+                    buttonStyle={{ marginBottom: 20, borderRadius: 30, style: { color: '#fff' }}} 
+                    title="Welcome to Elements"
+                    onPress={() => {
+                    }}
+                ></Button>
             </Header>
                 <Button buttonStyle={{borderRadius: 30, marginTop: 10, marginLeft: 10, marginRight: 10, marginBottom: 10, width: Dimensions.get('window').width * 0.95, height: Dimensions.get('window').width * 0.2}}
                 title="Go to FirstPage"
@@ -57,7 +56,7 @@ class HomeScreen extends React.Component{
           );
     }
 }
-export default HomeScreen;
+export default Home;
 
 
 
--- src/screens/firstpage.js
+++ src/screens/firstpage.js
@@ -1,34 +1,79 @@
 import React from 'react';
-import {ThemeProvider , Button} from 'react-native-elements';
+import {ThemeProvider , Button , Header , Icon , SearchBar} from 'react-native-elements';
 import {headerStyles as styles} from 'C:/elements/src/styles.js';
-import {TouchableOpacity , Text } from 'react-native';
+import {TouchableOpacity , Text , Dimensions} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 const theme ={
     Button: {
     titleStyle: {
     color: 'black',
     },
+    Header: {
+        barStyle:'light-content'
+    },
   },
 };
 
-class HomeScreen extends React.Component{
+state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+  const { search } = this.state;
+class firstpage extends React.Component{
     render (){
-       
+        
         return(  
-            <ThemeProvider theme = {theme} >
-                <Button title="Home">
-                onPress={() => {
+            <ThemeProvider theme = {theme}>
+            <Header
+                ViewComponent={LinearGradient} // Don't forget this!
+                linearGradientProps={{
+                colors: ['magenta', 'pink'],
+                start: { x: 0, y: 0.5 },
+                end: { x: 1, y: 0.5 },
+            }}> 
+                <Icon 
+                    name='arrow-back'
+                    iconStyle={{ icon: 'arrow-back', color: '#fff', marginBottom: 20 }} 
+                    onPress={() => {
                         this.props.navigation.navigate ('Home')           
-                    }}>
-                </Button>
+                    }}></Icon> 
+                <Icon 
+                    name='home'
+                    iconStyle={{ icon: 'home', color: '#fff', marginBottom: 20 }} 
+                    onPress={() => {
+                        this.props.navigation.navigate ('Home')           
+                    }}></Icon>          
+                <Icon 
+                    name='menu'
+                    iconStyle={{ icon: 'menu', color: '#fff', marginBottom: 20}} 
+                    onPress={() => {
+                        this.props.navigation.navigate ('firstpage')           
+                    }}></Icon>
                 
+                    
+            </Header>
+                <SearchBar 
+                    lightTheme= 'true'
+                    placeholderTextColor= "#fff"
+                    placeholder="Type Here..."
+                    onChangeText={this.updateSearch}
+                    value={search}
+                />
+            
             </ThemeProvider >  
+            
+           
            
 
           );
     }
 }
-export default HomeScreen;
+export default firstpage;
 
 
 
